1364287017bc46d576fe1ab649069b76d4c64f52
Abhinav Chatrathi <abhinavchatrathi@Abhinavs-MacBook-Air-2.local>
2026-03-23 14:50:28 +0530

    Error #11: Fix net_worth() to include property mortgage values

--- moneypoly/moneypoly/player.py
+++ moneypoly/moneypoly/player.py
@@ -33,7 +33,8 @@ class Player:
 
     def net_worth(self):
         """Calculate and return this player's total net worth."""
-        return self.balance
+        property_value = sum(p.mortgage_value for p in self.properties)
+        return self.balance + property_value
 
     def move(self, steps):
         """
